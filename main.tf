fd05c2f39e6dc9af9e2e09fda82cc20fb6d29593
tungUAS <tung.tt133@gmail.com>
2024-08-14 15:28:12 +0200

    add credentials

--- main.tf
+++ main.tf
@@ -10,6 +10,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  subscription_id = "f3201a06-2f4e-4b3b-9a7a-7a54898ea3d2"
+  client_id = "b67cb59f-b8b0-4a5e-8b04-76d0f67d3b08"
+  client_secret = "4A38Q~nAH~ZWoZFI7LwjpSUzgfmjqjnSYkF_Hbp9"
+  tenant_id = "4878e7ff-70ef-4ed9-8e6e-f13f1073f0ea"
 }
 
 resource "azurerm_resource_group" "example" {
