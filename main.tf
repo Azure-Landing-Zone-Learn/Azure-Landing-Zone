4cabefa3fa477987c1f29aa6b0d9c20c56235134
tungUAS <tung.tt133@gmail.com>
2024-08-18 19:51:10 +0200

    create new resource group

--- main.tf
+++ main.tf
@@ -9,17 +9,16 @@ terraform {
 }
 
 provider "azurerm" {
-
   features {}
 
-  subscription_id = "f3201a06-2f4e-4b3b-9a7a-7a54898ea3d2"
-  client_id       = "b67cb59f-b8b0-4a5e-8b04-76d0f67d3b08"
-  client_secret   = "4A38Q~nAH~ZWoZFI7LwjpSUzgfmjqjnSYkF_Hbp9"
-  tenant_id       = "4878e7ff-70ef-4ed9-8e6e-f13f1073f0ea"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 module "resource_group" {
-  source = "./resource_group"  # Adjust the path to your resource_group module if necessary
+  source = "./moudles/resource_group"  # Adjust the path to your resource_group module if necessary
 
   resource_group_name = "rg-eastasia-001"
   location            = var.location  # Use the default or specify another location
