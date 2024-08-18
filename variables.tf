ba16c65bbe447869055ec30d4cb864504717d050
tungUAS <tung.tt133@gmail.com>
2024-08-18 20:04:45 +0200

    create new resource group

--- variables.tf
+++ variables.tf
@@ -21,11 +21,6 @@ variable "tenant_id" {
   type        = string
 }
 
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
 variable "location" {
   description = "The location where the resource group will be created"
   type        = string
