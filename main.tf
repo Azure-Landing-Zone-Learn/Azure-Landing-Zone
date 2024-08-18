4c80ec2731f280901d9ef496ac2955ca1546c694
tungUAS <tung.tt133@gmail.com>
2024-08-18 21:01:25 +0200

    create vnet

--- main.tf
+++ main.tf
@@ -17,6 +17,13 @@ provider "azurerm" {
   tenant_id       = var.tenant_id
 }
 
+module "resource_group" {
+  source = "./modules/resource_group"  # Adjust the path to your resource_group module if necessary
+
+  resource_group_name = "rg-eastasia-001"
+  location            = var.location  # Use the default or specify another location
+}
+
 module "virtual_network" {
   source              = "./modules/virtual_network"
   vnet_name           = "vnet-eastasia-001"
