488cafb5525f8212fdf1b3a401ff67a9e39219bc
tungUAS <tung.tt133@gmail.com>
2024-08-18 21:07:16 +0200

    create vnet 2

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "resource_group" {
   location            = var.location  # Use the default or specify another location
 }
 
-module "virtual_network" {
+module "virtual_network_1" {
   source              = "./modules/virtual_network"
   vnet_name           = "vnet-eastasia-001"
   address_space       = ["10.0.0.0/16"]
@@ -39,6 +39,25 @@ module "virtual_network" {
     {
       name            = "subnet2"
       address_prefixes = ["10.0.2.0/24"]
+    }
+  ]
+}
+
+module "virtual_network_2" {
+  source              = "./modules/virtual_network"
+  vnet_name           = "vnet-eastasia-002"
+  address_space       = ["10.1.0.0/16"]  # Different address space to avoid overlap
+  location            = module.resource_group.resource_group_location
+  resource_group_name = module.resource_group.resource_group_name
+
+  subnets = [
+    {
+      name            = "subnet1"
+      address_prefixes = ["10.1.1.0/24"]
+    },
+    {
+      name            = "subnet2"
+      address_prefixes = ["10.1.2.0/24"]
     }
   ]
 }
