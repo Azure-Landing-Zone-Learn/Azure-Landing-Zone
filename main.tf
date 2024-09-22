c5c388d6eb4acd4f619fffaef8ccf9cb1fb09d30
tungUAS <tung.tt133@gmail.com>
2024-09-22 12:53:19 +0200

    test for in

--- main.tf
+++ main.tf
@@ -19,12 +19,37 @@ locals {
     terraform   = true
     environment = "Terraform Demo"
   }
+
+    subnets = [
+    {
+      name             = "subnet-abc-001"
+      address_prefixes = ["10.0.0.0/24"]
+    },
+    {
+      name             = "subnet-abc-${var.location}-002"
+      address_prefixes = ["10.0.1.0/24"]
+    },
+    {
+      name             = "subnet-abc-${var.location}-003"
+      address_prefixes = ["10.0.2.0/24"]
+    }
+  ]
+
+  list_test = [
+    "abc",
+    "test"
+  ]
 }
 
-module "connectivity_subscription" {
+output "test" {
+  value = [for s in local.local.list_test : upper(s)]
+}
+
+/* module "connectivity_subscription" {
   source = "./subscriptions/connectivity"
 
   location          = var.location
   subscription_name = var.subscription_connectivity_name
   tags              = local.tags
-}
+} */
+
---
+++ notice.md
@@ -0,0 +1,2 @@
+for_each only works with map, not list
+for ... in ... works with list
