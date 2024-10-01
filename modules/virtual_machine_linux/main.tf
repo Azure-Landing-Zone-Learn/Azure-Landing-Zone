7184bd29b8ca48dd8d1a887b7cf8f2cb017a35f6
tungUAS <tung.tt133@gmail.com>
2024-10-01 15:18:37 +0200

    add vms

--- modules/virtual_machine_linux/main.tf
+++ modules/virtual_machine_linux/main.tf
@@ -1,20 +1,10 @@
-module "nics" {
-  source = "../../modules/network_interface"
-
-  for_each            = var.nics
-  name                = each.value.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  subnet_id           = each.value.subnet_id
-  tags                = var.tags
-}
-
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
 
   network_interface_ids = [for nic in module.nics : nic.id]
+
   os_disk {
     name                 = var.os_disk_name
     caching              = var.caching
@@ -23,19 +13,20 @@ resource "azurerm_linux_virtual_machine" "linux_vm" {
   }
 
   size = var.size
-
   computer_name = var.computer_name
 
   admin_username = var.admin_username
-  admin_password = var.disable_password_authentication == false ? null : var.admin_password
+  admin_password = var.disable_password_authentication == false ? var.admin_password : null
 
+  # Ensure that SSH key is provided when password authentication is disabled
   dynamic "admin_ssh_key" {
-    for_each = var.admin_password == null ? [1] : []
+    for_each = var.disable_password_authentication == true ? [1] : []
     content {
       username   = var.admin_username
       public_key = var.public_key
     }
   }
+
   source_image_reference {
     publisher = var.publisher
     offer     = var.offer
