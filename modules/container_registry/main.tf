2cb4c45bfef40d3d52b14206141c84aa0f8e20aa
tungUAS <tung.tt133@gmail.com>
2024-10-18 20:23:06 +0200

    add private acr

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -36,7 +36,7 @@ module "private_dns_zone" {
 resource "azurerm_private_dns_zone_virtual_network_link" "vnet_link" {
   name                  = "acr-dns-link-${var.name}"
   resource_group_name   = var.resource_group_name
-  private_dns_zone_name = module.private_dns_zone.name
+  private_dns_zone_name = "${var.private_dns_zone_name}.${var.name}.io"
   virtual_network_id    = var.vnet_id
 }
 
