dd1471f7504b7db1b18fdfdc0d5da362475f3bf9
tungUAS <tung.tt133@gmail.com>
2024-10-18 22:28:26 +0200

    remove unused rt

--- subscriptions/connectivity/main.tf
+++ subscriptions/connectivity/main.tf
@@ -1,34 +1,5 @@
 locals {
-  routes = [
-    {
-      name           = "route1"
-      address_prefix = "10.0.0.0/24"
-      next_hop_type  = "VirtualNetworkGateway"
-    },
-    {
-      name                   = "route2"
-      address_prefix         = "10.0.1.0/24"
-      next_hop_type          = "VirtualAppliance"
-      next_hop_in_ip_address = "0.0.0.0"
-    }
-  ]
-
   subnets = [
-    {
-      name             = "subnet-${var.subscription_name}-${var.location}-001"
-      address_prefixes = ["10.0.0.0/24"]
-      route_table_id   = module.rt.id
-    },
-    {
-      name                      = "subnet-${var.subscription_name}-${var.location}-002"
-      address_prefixes          = ["10.0.1.0/24"]
-      network_security_group_id = module.nsg.id
-    },
-    {
-      name                      = "subnet-${var.subscription_name}-${var.location}-003"
-      address_prefixes          = ["10.0.2.0/24"]
-      network_security_group_id = module.nsg.id
-    },
     {
       name             = "AzureBastionSubnet"
       address_prefixes = ["10.0.3.0/26"]
@@ -36,20 +7,6 @@ locals {
     {
       name             = "AzureFirewallSubnet"
       address_prefixes = ["10.0.4.0/24"]
-    }
-  ]
-
-  security_rules = [
-    {
-      name                       = "AllowMyIpAddressCustom8080Inbound"
-      protocol                   = "*"
-      access                     = "Allow"
-      priority                   = 100
-      direction                  = "Inbound"
-      source_address_prefix      = "89.244.82.247"
-      destination_address_prefix = "*"
-      source_port_range          = "*"
-      destination_port_range     = "8080"
     }
   ]
 
@@ -77,14 +34,6 @@ locals {
 
 resource "azurerm_public_ip" "fw_pip" {
   name                = var.fw_pip_name
-  location            = var.location
-  resource_group_name = module.rg.name
-  allocation_method   = var.allocation_method
-  sku                 = var.sku_pip
-}
-
-resource "azurerm_public_ip" "bastion_pip" {
-  name                = var.bastion_pip_name
   location            = var.location
   resource_group_name = module.rg.name
   allocation_method   = var.allocation_method
@@ -121,7 +70,7 @@ module "firewall" {
   ip_configuration = {
     name                 = var.ip_configuration_name
     subnet_id            = module.vnet.subnets["AzureFirewallSubnet"]
-    public_ip_address_id = azurerm_public_ip.bastion_pip.id
+    public_ip_address_id = azurerm_public_ip.fw_pip.id
   }
 }
 
@@ -134,26 +83,6 @@ module "basic_bastion" {
   resource_group_name       = module.rg.name
   sku                       = "Basic"
   virtual_network_subnet_id = module.vnet.subnets["AzureBastionSubnet"]
-}
-
-module "rt" {
-  source = "../../modules/route_table"
-
-  name                = "rt-${var.subscription_name}-${var.location}-001"
-  location            = var.location
-  resource_group_name = module.rg.name
-  routes              = { for route in local.routes : route.name => route }
-  tags                = var.tags
-}
-
-module "nsg" {
-  source = "../../modules/network_security_group"
-
-  name           = "nsg-${var.subscription_name}-${var.location}-001"
-  location       = var.location
-  resource_group = module.rg.name
-  tags           = var.tags
-  security_rules = { for rule in local.security_rules : rule.name => rule }
 }
 
 output "vnet_id" {
