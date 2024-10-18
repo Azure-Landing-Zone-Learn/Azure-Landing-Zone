bb30a150fd223c8b11bca309af53ba5b35434400
tungUAS <tung.tt133@gmail.com>
2024-10-18 14:05:24 +0200

    add private acr

--- modules/container_registry/main.tf
+++ modules/container_registry/main.tf
@@ -1,5 +1,13 @@
 # private acr
 
+resource "azurerm_container_registry" "acr" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  admin_enabled       = var.admin_enabled
+}
+
 module "pe" {
   source              = "../../modules/private_endpoint"
   name                = var.pe_name
@@ -13,12 +21,4 @@ module "pe" {
         private_connection_resource_id = var.private_service_connection.private_connection_resource_id
         is_manual_connection           = var.private_service_connection.is_manual_connection
     }
-}
-
-resource "azurerm_container_registry" "acr" {
-  name                = var.name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  sku                 = var.sku
-  admin_enabled       = var.admin_enabled
 }
