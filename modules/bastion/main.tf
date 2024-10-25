64df1e772b8cb8e9ace22e45dcdd9dc5d23fe0ba
tungUAS <tung.tt133@gmail.com>
2024-10-25 09:58:50 +0200

    refactor bastion module

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,13 +1,11 @@
 module "pip" {
-  source = "../../modules/public_ip"
-
+  source              = "../../modules/public_ip"
+  count               = var.sku != "Developer" ? 1 : 0
   name                = var.public_ip_address_name
   location            = var.location
   resource_group_name = var.resource_group_name
   sku                 = var.sku_pip
   allocation_method   = "Static"
-
-  count = var.sku != "Developer" ? 1 : 0
 }
 
 resource "azurerm_bastion_host" "bastion" {
@@ -17,9 +15,12 @@ resource "azurerm_bastion_host" "bastion" {
   sku                 = var.sku
   virtual_network_id  = var.virtual_network_id
 
-  ip_configuration {
-    name                 = var.ip_configuration_name
-    public_ip_address_id = length(module.pip) > 0 ? module.pip[0].id : null
-    subnet_id            = var.virtual_network_subnet_id
+  dynamic "ip_configuration" {
+    for_each = var.sku != "Developer" ? [1] : []
+    content {
+      name                 = var.ip_configuration_name
+      public_ip_address_id = length(module.pip) > 0 ? module.pip[0].id : null
+      subnet_id            = var.virtual_network_subnet_id
+    }
   }
 }
