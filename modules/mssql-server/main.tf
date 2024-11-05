22ac4d122e4c671481c9073f24cc1dee1d158180
tungUAS <tung.tt133@gmail.com>
2024-11-05 15:56:37 +0100

    add pes

--- modules/mssql-server/main.tf
+++ modules/mssql-server/main.tf
@@ -29,7 +29,7 @@ resource "azurerm_mssql_server" "server" {
 }
 
 resource "azurerm_private_endpoint" "sql_private_endpoints" {
-  for_each = var.subnet_ids 
+  for_each = toset(var.subnet_ids)
 
   name                = "pe-${each.value}-${each.key}"  # Unique name for each PE
   location            = var.location
